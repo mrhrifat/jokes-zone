92987559e49b1ab855a065b3e322d69d7b717e43
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-13 11:23:31 +0600

    Add comment

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,17 @@
+// API URL
 const url = "https://icanhazdadjoke.com/slack"
+
+// Promise 
 fetch(url)
     .then(res => res.json())
     .then(joke => {
+
+        // Output 
         const output = document.getElementById('paragraph')
+
+        // Access Jokes
         const getJoke = joke.attachments[0].text
+
+        // Show Jokes
         output.innerText = getJoke
     })
